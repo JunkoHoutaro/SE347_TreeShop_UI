31cbb86010223aa5122889d85cb07bca871786f4
Nguyễn Phú Hào <21520223@gm.uit.edu.vn>
2024-12-31 06:39:27 +0700

    Delete StaffUI_TreeShop directory

--- StaffUI_TreeShop/src/layouts/components/Header/index.jsx
+++
@@ -1,9 +0,0 @@
-function Header({ children }) {
-    return (
-        <header className="flex h-8 select-none items-center justify-center  font-medium text-slate-900">
-            {children}
-        </header>
-    );
-}
-
-export default Header;
