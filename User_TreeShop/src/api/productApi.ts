df12c10296ca40854424a27b9917cbddbd1b8159
Nguyễn Phú Hào <21520223@gm.uit.edu.vn>
2024-12-31 06:40:07 +0700

    Delete User_TreeShop directory

--- User_TreeShop/src/api/productApi.ts
+++
@@ -1,7 +0,0 @@
-// import { ResponeData } from "../redux/product/saga";
-
-export const getListProduct = async () => {
-    const respone = await fetch('http://localhost:5000/api/product')
-            .then((res) => res.json());
-    return respone;
-}
